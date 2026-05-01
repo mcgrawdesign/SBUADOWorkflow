27c28b288feead4d1bf045ed40b8bc0d41f07996
Bradley McGraw <Bradley.McGraw@microsoft.com>
2026-04-30 21:54:35 -0600

    test push

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
+    base: '/SBUADOWorkflow/',
+    server: {
     host: "::",
     port: 8080,
     hmr: {
